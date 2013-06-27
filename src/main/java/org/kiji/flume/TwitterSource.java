5d88e5f21c1ed52d754bb6592d6eb78b61b267d0
Robert Chu <robert@wibidata.com>
2013-06-26 18:31:24 -0700

    Adds TwitterSource.

---
+++ src/main/java/org/kiji/flume/TwitterSource.java
@@ -0,0 +1,104 @@
+package org.kiji.flume;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.PollableSource;
+import org.apache.flume.channel.ChannelProcessor;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.AbstractSource;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.TwitterApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.kiji.schema.KijiIOException;
+
+/**
+ */
+public class TwitterSource extends AbstractSource implements Configurable, PollableSource {
+  private static final Logger LOG = LoggerFactory.getLogger(TwitterSource.class);
+  public static final String API_KEY_PROPERTY = "apikey";
+  public static final String API_SECRET_PROPERTY = "apisecret";
+  public static final String TOKEN_PROPERTY = "token";
+  public static final String TOKEN_SECRET = "tokensecret";
+  public static final String TWITTER_URL = "https://stream.twitter.com/1.1/statuses/sample.json";
+
+  private OAuthRequest mRequest;
+  private BufferedReader mReader;
+
+  @Override
+  public void configure(Context context) {
+    // Get the required secrets.
+    final String apiKey = System.getProperty(API_KEY_PROPERTY);
+    final String apiSecret = System.getProperty(API_SECRET_PROPERTY);
+    final String accessToken = System.getProperty(TOKEN_PROPERTY);
+    final String accessTokenSecret = System.getProperty(TOKEN_SECRET);
+    LOG.info("API Key: {}", apiKey);
+    LOG.info("API Secret: {}", apiSecret);
+    LOG.info("Access Token: {}", accessToken);
+    LOG.info("Access Token Secret: {}", accessTokenSecret);
+
+    // Setup the connection with OAuth.
+    final OAuthService service = new ServiceBuilder()
+        .provider(TwitterApi.class)
+        .apiKey(apiKey)
+        .apiSecret(apiSecret)
+        .build();
+    final Token token = new Token(accessToken, accessTokenSecret);
+    mRequest = new OAuthRequest(Verb.POST, TWITTER_URL);
+    service.signRequest(token, mRequest);
+  }
+
+  @Override
+  public void start() {
+    // Open up a reader.
+    final Response response = mRequest.send();
+    mReader = new BufferedReader(new InputStreamReader(response.getStream()));
+  }
+
+  @Override
+  public void stop() {
+    try {
+      mReader.close();
+    } catch (IOException ioe) {
+      throw new KijiIOException(ioe);
+    }
+  }
+
+  @Override
+  public Status process() throws EventDeliveryException {
+    final ChannelProcessor channelProcessor = getChannelProcessor();
+
+    // Read the next line.
+    String line;
+    try {
+      line = mReader.readLine();
+    } catch (IOException ioe) {
+      throw new KijiIOException(ioe);
+    }
+
+    // Process the line.
+    if (line != null) {
+      //LOG.info(line);
+
+      final Event event = EventBuilder.withBody(line, Charset.forName("UTF-8"));
+      channelProcessor.processEvent(event);
+
+      return Status.READY;
+    } else {
+      return Status.BACKOFF;
+    }
+  }
+}
